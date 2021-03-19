4e9d61205660b7abaab99941845ffb3810dcc0c9
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-03-19 11:56:26 +0200

    Update Task.0.10.py

--- Task.0.10.py
+++ Task.0.10.py
@@ -1,19 +1,18 @@
-def match_up():
+def match_up(first_word, second_word):
     
-    first_input = input("Please Enter The First Word:" )
-    second_input = input("Please Enter The Second Word:" )
+    
     first_input_values = ""
     second_input_values= ""
 
-    for i in first_input:
+    for i in first_word:
         first_input_values += i
-    for i in second_input:
+    for i in second_word:
          second_input_values += i
 
 
     first_elements = ""
 
-    for i in first_input:
+    for i in first_word:
         if i in second_input_values:
             first_elements += i
     print("First Match Ups:", ", ".join(first_elements))
@@ -21,9 +20,9 @@ def match_up():
     
     second_elements = ""
 
-    for i in second_input:
+    for i in second_word:
         if i in first_input_values:
             second_elements += i
     print("Second Match Ups:", ", ".join (second_elements))
 
-match_up()
+match_up("strong", "strange")
