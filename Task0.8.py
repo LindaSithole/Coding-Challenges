49b96aa8ab0574742fcd03f4df44a10718372719
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-03-22 12:10:31 +0200

    Update Task0.8.py

--- Task0.8.py
+++ Task0.8.py
@@ -1,16 +1,17 @@
 
-def hours_into_minutes():
+def hours_into_minutes(number):
 
-    number = int(input("Please Enter Number:"))
+  
     hour = number // 60
     hr = str(hour)
     minute = number % 60
     mnt = str(minute)
 
-    if number <= 120 and minute <= 1:
-        return hr + "hour," + mnt + "minute"
+    if number <= 60 and minute <= 1:
+        print(hr + "hour," + mnt + "minute")
 
-    elif number <= 120 and minute > 1:
-         return hr + "hour," + mnt + "minute"
+    elif number >= 60 and minute >= 1:
+        print(hr + "hour," + mnt + "minute")
+              
 
-print(hours_into_minutes())
+hours_into_minutes(71)
