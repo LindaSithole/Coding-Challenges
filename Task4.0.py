ef0eeddc3dc7cccca15924a9e62df6182c154089
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-03-25 01:04:01 +0200

    Update Task4.0.py

--- Task4.0.py
+++ Task4.0.py
@@ -1,11 +1,9 @@
 def even_or_odd(values):
-   
-    even_numbers = [1,2,4,6,8,10]
-    odd_numbers = [1,3,5,7,9,11]
 
-    if values in even_numbers: 
+    if (values % 2) == 0:
         return "even"
-    elif values in odd_numbers:
+    else:
         return "odd"
 
-print(even_or_odd(4))
+
+print(even_or_odd(199))
