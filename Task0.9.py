9b6d4e9995650669fafc968609fd20df8a78936d
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-09-10 12:16:29 +0200

    Update Task0.9.py

--- Task0.9.py
+++ Task0.9.py
@@ -1,15 +1,30 @@
-def print_vowels(word):
+package coding.challenges;
 
+/**
+ *
+ * @author Student
+ */
+public class Vowels {
+  
+	public static void main(String[] args) {
+		
+		String str = "Umuzi";
+	
+		System.out.print("Vowels :");
+		
+		for (int i = 0; i < str.length(); i++) {
+		
+			if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'
+					|| str.charAt(i) == 'u' || str.charAt(i) == 'A' || str.charAt(i) == 'E' || str.charAt(i) == 'I'
+                                            || str.charAt(i) == 'O' || str.charAt(i) == 'U') {
+				
+				System.out.print(" " + str.charAt(i));
+			}
+		}
 
-    vowels = "a", "e", "i", "o", "u"
-    occurrences = ""
+	}
 
-    for i in word:
-        if i in vowels:
-            occurrences += i
-
-    print("vowels:", ", ".join(occurrences))
-
-
-print_vowels("Umuzi")
+}
+                        
+      
 
