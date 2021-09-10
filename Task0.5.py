34d5e179b3058c6369d670d286dfa0f7d267179e
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-09-10 12:29:31 +0200

    Update Task0.5.py

--- Task0.5.py
+++ Task0.5.py
@@ -1,9 +1,29 @@
-def area__of_triangle(a, b, c):
+package javaapplication15;
 
-    semi_p = (a + b + c) / 2
+/**
+ *
+ * @author Student
+ */
+public class JavaApplication15 {
 
-    area = (semi_p * (semi_p - a) * (semi_p - b) * (semi_p - c)) ** 0.5
-    return round(area)
+    static double area(double h, double b)
+    {
+     
+        return (h * b) / 2;
+    }
 
+    public static void main(String[] args)
+    {
+       
+        double h = 10;
+  
+        
+        double b = 15;
+  
+    
+        System.out.println("Area of the triangle: "
+                           + area(h, b));
+    }
+}
 
-print(area__of_triangle(5, 6, 7))
+    
