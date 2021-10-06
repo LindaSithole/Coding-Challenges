0544bb231d5333de0ad9579d479d1cc592324be4
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-10-07 00:41:10 +0200

    Update vowels.java

--- Level Zero/Task0.9/src/vowels.java
+++ Level Zero/Task0.9/src/vowels.java
@@ -3,8 +3,8 @@ public class pseudocode {
     public static void  main(String[] args) {
         
         String str = "Umuzi";
-
         System.out.print("Vowels:");
+        
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'
                     || str.charAt(i) == 'u' || str.charAt(i) == 'A' || str.charAt(i) == 'E' || str.charAt(i) == 'I'
