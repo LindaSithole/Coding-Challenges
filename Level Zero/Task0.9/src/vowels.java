7dff23c3fbd1d1cdc52581c3f8d5de57f29150d5
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-10-06 16:56:06 +0200

    Update vowels.java

--- Level Zero/Task0.9/src/vowels.java
+++ Level Zero/Task0.9/src/vowels.java
@@ -1,24 +1,16 @@
-public class vowels {
+public class pseudocode {
 
-    public static class Vowels {
+    public static void  main(String[] args) {
+        
+        String str = "Umuzi";
 
-        public static void main(String[] args) {
-
-            String str = "Umuzi";
-
-            System.out.print("Vowels :");
-
-            for (int i = 0; i < str.length(); i++) {
-
-                if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'
-                        || str.charAt(i) == 'u' || str.charAt(i) == 'A' || str.charAt(i) == 'E' || str.charAt(i) == 'I'
-                        || str.charAt(i) == 'O' || str.charAt(i) == 'U') {
-
-                    System.out.print(" " + str.charAt(i));
-                }
+        System.out.print("Vowels:");
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'
+                    || str.charAt(i) == 'u' || str.charAt(i) == 'A' || str.charAt(i) == 'E' || str.charAt(i) == 'I'
+                    || str.charAt(i) == 'O' || str.charAt(i) == 'U') {
+                System.out.print(" " + str.charAt(i));
             }
-
         }
-
     }
 }
