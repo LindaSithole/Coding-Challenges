67fa93ef76fb13b77625c618056ab3ea780d68b7
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-10-07 01:40:02 +0200

    Update convention.java

--- Level Zero/Task0.8/src/convention.java
+++ Level Zero/Task0.8/src/convention.java
@@ -1,21 +1,14 @@
-import java.util.Scanner;
+public class Task8 {
 
-public class convention {
+public static void Conversion(int fullSeconds) {
+    
+    int hours = fullSeconds % 3600 / 60;
+    int  minutes = fullSeconds % 3600 % 60;
+    
+    System.out.println(hours + "Hour(s)" + minutes + "minute(s)" );
+}
+    public static void main (String[] args){
 
-
-        public static void main (String[] args){
-
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter Number"  );
-
-            int sec = sc .nextInt();
-            int mins = sec % 3600 / 60;
-            int hr = sec / 3600;
-            int s2 = sec %3600%60;
-
-
-            System.out.println(hr + "Hour(s)" + mins +"minute(s)"+s2+"Second(s)");
-
-        }
-
+        Conversion(117);
+    }
 }
