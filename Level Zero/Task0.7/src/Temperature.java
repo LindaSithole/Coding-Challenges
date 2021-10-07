1c2d4820c1fec9f2f1d2b0865ece07ced35f743a
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-10-07 02:06:52 +0200

    Update Temperature.java

--- Level Zero/Task0.7/src/Temperature.java
+++ Level Zero/Task0.7/src/Temperature.java
@@ -1,34 +1,18 @@
-import java.util.Scanner;
-public class Temperature {
-
-
-
-    public static class Temp {
-
-        public static void main(String[] args) {
-
-            double a,c;
-            Scanner sc=new Scanner(System.in);
-            System.out.println("Enter  Fahrenheit temperature");
-            a=sc.nextDouble();
-            System.out.println("Celsius temperature is = "+celsius(a));
+class Task7 {
+    public static void main(String[] args) {
+        
+            celsius(5);
+            fahrenheit(8);
         }
-        static double celsius(double f)
-        {
-            return  (f-32)*5/9;
-        }
+        static void celsius(double  temperature){
 
-        public static void secondFunction(String arg[])
-        {
-            double a,c;
-            Scanner sc=new Scanner(System.in);
-            System.out.println("Enter  Celsius temperature");
-            a=sc.nextDouble();
-            System.out.println("Fahrenheit temperature is = "+fahrenheit(a));
+            temperature = ((temperature-32)*5/9);
+            System.out.println(" fahrenheit= " + temperature+"°F");
         }
-        static double fahrenheit(double c)
-        {
-            return  ((9*c)/5)+32;
-        }
+    static void fahrenheit(double  temperature){
+
+        temperature = ((temperature - 32)*5)/9;
+        System.out.println(" Celsius = " + temperature+"°C");
     }
 }
+
