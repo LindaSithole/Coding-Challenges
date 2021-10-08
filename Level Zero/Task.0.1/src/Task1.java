77d7f7f84f944e314e55ff4f8786d6fcf88b8a3d
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-10-08 10:43:17 +0200

    Update Task1.java

--- Level Zero/Task.0.1/src/Task1.java
+++ Level Zero/Task.0.1/src/Task1.java
@@ -1,15 +1,15 @@
 public class Task1 {
 
-        public static void main(String[] args) {
-
-            int x = 0;
-            int y = 1;
-     
-            System.out.println(x);
-            System.out.println(y);
-            System.out.println(x+3);
-            System.out.println(y+3);
-        }
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 1;
+        System.out.println(x);
+        System.out.println(y);
+         x = x + 3;
+         y = y + x;
+        System.out.println(x);
+        System.out.println(y);
     }
+}
 
     
