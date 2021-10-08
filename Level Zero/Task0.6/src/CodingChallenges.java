d0ded47b215eb4951455d180bf6710dbe7d34984
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-10-08 13:14:04 +0200

    Update CodingChallenges.java

--- Level Zero/Task0.6/src/CodingChallenges.java
+++ Level Zero/Task0.6/src/CodingChallenges.java
@@ -1,16 +1,14 @@
-public class Task6 {
-    
+public class Task7{
     public static void main(String[] args){
         
-       numbers(2,3,4) ;
+        numbers(2,3,4);
     }
-    static void numbers (int num1, int num2, int num3 ){
- 
+    static int numbers (int num1, int num2, int num3 ){
         if (num1 >= num2 && num1 >= num3)
-            System.out.println( num1 );
+            return(num1);
         else if (num2 >= num1 && num2 >= num3)
-            System.out.println( num2 );
+            return(num2);
         else
-            System.out.println( num3 );
-    }
+            return num3;
+            }
 }
