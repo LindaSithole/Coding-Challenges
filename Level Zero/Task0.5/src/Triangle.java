285dec04c98f2b9bcb1f4bc9a048433f91cab11a
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-10-11 01:52:42 +0200

    Update Triangle.java

--- Level Zero/Task0.5/src/Triangle.java
+++ Level Zero/Task0.5/src/Triangle.java
@@ -1,13 +1,14 @@
 
 public class Task5{
-        static double Area(double a, double b, double c)
-        {
-            double semiparameter = (a + b + c) / 2;
-            double area = (int)Math.sqrt((semiparameter)*(semiparameter-a)*(semiparameter-b)*(semiparameter-c));
-            return area;
-        }
-        public static void main(String[] args)
-        {
-            return("Area of the triangle: " + Area(3,4,5));
-        }
+    static double TriangleArea(double a, double b, double c)
+    {
+        double semiparameter = (a + b + c) / 2;
+        double area = (int)Math.sqrt((semiparameter)*(semiparameter-a)*(semiparameter-b)*(semiparameter-c));
+        return area;
+    }
+    public static void main(String[] args)
+    {
+        System.out.print("Area of the triangle: " + TriangleArea(3,4,5));
+    }
 }
+
