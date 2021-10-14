923a216cff92a06fe460dc85de8c50d06901f5e4
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-10-15 01:07:59 +0200

    Update Temperature.java

--- Level Zero/Task0.7/src/Temperature.java
+++ Level Zero/Task0.7/src/Temperature.java
@@ -1,15 +1,15 @@
 class Task7 {
     public static void main(String[] args) {
         
-            celsius(5);
-            fahrenheit(8);
-        }
-        static boolean celsius(double  temperature){
+        celsius(5);
+        fahrenheit(8);
+    }
+    static boolean celsius(double  temperature){
 
-            temperature = ((temperature-32)*5/9);
-            System.out.println(" fahrenheit= " + temperature+"°F");
-            return true;
-        }
+        temperature = ((temperature-32)*5/9);
+        System.out.println(" fahrenheit= " + temperature+"°F");
+        return true;
+    }
     static boolean fahrenheit(double  temperature){
 
         temperature = ((temperature - 32)*5)/9;
