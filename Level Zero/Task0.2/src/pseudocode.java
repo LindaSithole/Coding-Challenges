aaf052975d87d3d78acf20368272010b7181df7f
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-10-15 11:02:53 +0200

    Update pseudocode.java

--- Level Zero/Task0.2/src/pseudocode.java
+++ Level Zero/Task0.2/src/pseudocode.java
@@ -9,7 +9,6 @@ public class pseudocode {
         int b = (1 + 1 * 2 ) /  2;
         System.out.println(x);
         System.out.println(y);
-
         System.out.println(z);
         System.out.println(a);
         System.out.println(b);
