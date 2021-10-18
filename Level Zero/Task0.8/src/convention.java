b6a75b30be385e47a50a2287c6ba06aef867c2f9
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-10-18 12:07:34 +0200

    Update convention.java

--- Level Zero/Task0.8/src/convention.java
+++ Level Zero/Task0.8/src/convention.java
@@ -1,24 +1,22 @@
-
 class Task8 {
 
     public static void TimeConversion(int fullSeconds) {
-
         int hours = fullSeconds % 3600 / 60;
-          if (hours >= 2) {
-              System.out.print(hours + "Hours,");
-          }else {
-              System.out.print(hours + "Hour,");
-          }
+            if (hours >= 2 || hours == 0) {
+                System.out.print(hours + "Hours,");
+            }else {
+                System.out.print(hours + "Hour,");
+            }
         int  minutes = fullSeconds % 3600 % 60;
-        if (minutes >= 2) {
-            System.out.print(minutes + "minutes");
-        }else {
-            System.out.print(hours + "minute");
-        }
-    }
+            if (minutes >= 2 || minutes == 0) {
+                System.out.print(minutes + "minutes");
+            }else {
+                System.out.print(hours + "minute");
+            }
+            }
     public static void main (String[] args){
 
-        TimeConversion(71);
+        TimeConversion(0);
     }
 }
 
