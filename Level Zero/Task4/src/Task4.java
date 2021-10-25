b85aa9b0ecea4e435983cb2b7a9b8224b0197e95
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-10-25 13:38:55 +0200

    Update Task4.java

--- Level Zero/Task4/src/Task4.java
+++ Level Zero/Task4/src/Task4.java
@@ -1,14 +1,13 @@
 class Task4 {
     public static void EvenOrOdd(int num) {
-        if (num%2==0) {
+        if (num % 2 == 0) {
             System.out.println("Even");
         } else {
-            if (num%2!=0)  {
             System.out.println("Odd");
         }
             }
-    }
 
     public static void main(String[] args) {
         EvenOrOdd(5);
-    }}
+    }
+}
