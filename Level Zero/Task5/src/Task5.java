7ea2261467cb06e422185cc27ea9796318cfca74
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-10-25 13:46:26 +0200

    Update Task5.java

--- Level Zero/Task5/src/Task5.java
+++ Level Zero/Task5/src/Task5.java
@@ -3,7 +3,7 @@ public class Task5{
     static double TriangleArea(double a, double b, double c)
     {
         double semiparameter = (a + b + c) / 2;
-        double area = (int)Math.sqrt((semiparameter)*(semiparameter-a)*(semiparameter-b)*(semiparameter-c));
+        double area = (double)Math.sqrt((semiparameter)*(semiparameter-a)*(semiparameter-b)*(semiparameter-c));
         return area;
     }
     public static void main(String[] args)
