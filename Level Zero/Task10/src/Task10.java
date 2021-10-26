30168c6777d348e75c5310b58b80174951f5bdc6
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-10-26 15:53:34 +0200

    Update Task10.java

--- Level Zero/Task10/src/Task10.java
+++ Level Zero/Task10/src/Task10.java
@@ -1,32 +1,26 @@
 public class Task10 {
 
-        public static void common_chars(String str_1, String str_2){
-            int[] array_1 = new int[26];
-            int[] array_2 = new int[26];
-            int str_len_1 = str_1.length();
-            int str_len_2 = str_2.length();
-            System.out.print("Vowels:" + " ");
-            for (int i = 0 ; i < str_len_1 ; i++)
-                array_1[str_1.charAt(i) - 'a'] += 1;
-            for (int i = 0 ; i < str_len_2 ; i++)
-                array_2[str_2.charAt(i) - 'a'] += 1;
-            for (int i = 0 ; i < 26 ; i++){
-                if (array_1[i] != 0 && array_2[i] != 0){
-                    for (int j = 0; j < Math.min(array_1[i], array_2[i]) ; j++) str_1 = str_1.substring(0, str_1.length() -1);
-                    str_2 = str_2.substring(0, str_2.length() -1);
-                        System.out.print(((char )(i +'a')+",")) ;
+    public static void common_chars(String string1, String string2) {
+     
+        String string3 = string1, string4 = string2, commonLetter = "";
+        System.out.print("Common Characters:" + " ");
+
+        if (string1.length() > string2.length()) {
+            string3 = string2;
+            string4 = string1;
         }
-}
+        for (int x = 0; x < string3.length(); x++) {
+            String str5 = Character.toString(string3.charAt(x));
 
+            for (int y = 0; y < string4.length(); y++) {
+                String string6 = Character.toString(string4.charAt(y));
+
+                if (str5.equals(string6)) {
+                    commonLetter += str5 + ",";
+                }
+            }
         }
-
-
-    public static void main(String[] args) {
-
-        String my_str_1 = "house";
-        String my_str_2 = "computers";
-
-        common_chars(my_str_1, my_str_2);
+        commonLetter = commonLetter.substring(0, commonLetter.length() - 1);
+        System.out.println(commonLetter);
     }
 }
-
