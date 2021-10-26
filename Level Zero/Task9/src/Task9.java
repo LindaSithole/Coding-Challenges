e60185f6fcffc9ba2f27834b70406b774f3b414c
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-10-26 15:54:44 +0200

    Update Task9.java

--- Level Zero/Task9/src/Task9.java
+++ Level Zero/Task9/src/Task9.java
@@ -2,19 +2,19 @@
 class Task9 {
     
     public static void main(String[] args) {
-
         String str = "Umuzi";
         System.out.print("Vowels:" + " ");
+        StringBuilder string1 = new StringBuilder();
 
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'
-                    || str.charAt(i) == 'u') {
-
-                System.out.print(((str.charAt(i) + ",")));
-
-
+                    || str.charAt(i) == 'u')
+            {
+                string1.append(str.charAt(i)).append(",");
             }
         }
+        String result = string1.substring(0, string1.length()-1);
+        System.out.print(result);
     }
 }
 
