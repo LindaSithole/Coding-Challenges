ca4f38bcb50dd80a6cc3c571658f49b215ebcb32
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-11-08 11:31:02 +0200

    Update Task6.java

--- Level Zero/Task6/src/Task6.java
+++ Level Zero/Task6/src/Task6.java
@@ -1,18 +1,18 @@
 class Task6 {
-    public static void main(String[] args){
-        maximNumber(6,3,4);
+    public static void main(String[] args) {
     }
-    static int maximNumber(int num1, int num2, int num3 ){
+
+    static int maximNumber(int num1, int num2, int num3) {
         int max;
         if (num1 > num2 && num1 > num3) {
-                max = num1;
+            max = num1;
         } else {
             if (num2 > num3)
                 max = num2;
             else
                 max = num3;
         }
-        System.out.println("Largest Number : " + max);
         return max;
     }
 }
+
