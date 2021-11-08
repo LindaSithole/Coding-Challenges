78689d91861f31ed4b11e33b951203283b84f55d
LindaSithole <73214301+LindaSithole@users.noreply.github.com>
2021-11-08 11:33:02 +0200

    Update Task7.java

--- Level Zero/Task7/src/Task7.java
+++ Level Zero/Task7/src/Task7.java
@@ -1,17 +1,16 @@
 class Task7 {
     public static void main(String[] args) {
-        CelsiusConversion(5);
-        FahrenheitConversion(8);
+        celsiusConversion(5);
+        fahrenheitConversion(8);
     }
-    static double CelsiusConversion(double temperature){
+    static double celsiusConversion(double temperature){
         temperature = ((temperature * 1.8) + 32);
-        System.out.println(" fahrenheit= " + temperature+"°F");
+
         return temperature;
     }
-    static double FahrenheitConversion(double temperature){
+    static double fahrenheitConversion(double temperature){
 
         temperature = ((temperature - 32)*5)/9;
-        System.out.println(" Celsius = " + temperature+"°C");
         return temperature;
     }
 }
